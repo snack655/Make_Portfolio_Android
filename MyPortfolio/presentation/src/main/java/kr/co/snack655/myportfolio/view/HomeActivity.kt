ce6738fd35b68981961302a6c894d5aec280b64d
snack655 <80940200+snack655@users.noreply.github.com>
2022-01-22 01:41:19 +0900

    Add : HomeActivity and connect BaseActivity
    2022-01-22

---
+++ MyPortfolio/presentation/src/main/java/kr/co/snack655/myportfolio/view/HomeActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.snack655.myportfolio.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import kr.co.snack655.myportfolio.base.BaseActivity
+import kr.co.snack655.myportfolio.databinding.ActivityHomeBinding
+import kr.co.snack655.myportfolio.viewmodel.activity.HomeViewModel
+
+class HomeActivity() : BaseActivity<ActivityHomeBinding, HomeViewModel>() {
+
+    override val viewModel: HomeViewModel by viewModels()
+
+    override fun observerViewModel() {
+
+    }
+}
---
+++ MyPortfolio/presentation/src/main/java/kr/co/snack655/myportfolio/viewmodel/activity/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package kr.co.snack655.myportfolio.viewmodel.activity
+
+import kr.co.snack655.myportfolio.base.BaseViewModel
+import kr.co.snack655.myportfolio.util.SingleLiveEvent
+
+class HomeViewModel: BaseViewModel() {
+
+}
